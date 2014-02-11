fdc39baf534926a34e783b9ed066020bda0796ce
Heather Lo <hlo@email.arizona.edu>
2014-02-11 11:36:29 -0700

    #18 creating Ruby uninstall scripts

---
+++ components/ruby/ruby-2.0.0-p247/_uninstall.sh
@@ -0,0 +1,36 @@
+#
+# component uninstall script for ruby-2.0.0-p247
+#
+
+#
+# Remove the component build and log directories.
+
+echo "[-uninstall-] Removing ${COMPONENT_BUILDSRCDIR} ..."
+rm -rf ${COMPONENT_BUILDSRCDIR}
+
+echo "[-uninstall-] Removing ${COMPONENT_BUILDLOGSDIR} ..."
+rm -rf ${COMPONENT_BUILDLOGSDIR}
+
+#
+# Remove component home directory. (/opt/kuali/ruby)
+
+echo "[-uninstall-] Removing ${COMPONENT_INSTALLDIR} ..."
+rm -rf ${COMPONENT_INSTALLDIR}
+
+#
+# Remove component configuration directory. (/etc/opt/kuali/ruby)
+
+echo "[-uninstall-] Removing ${COMPONENT_CONFIGDIR} ..."
+rm -rf ${COMPONENT_CONFIGDIR}
+
+#
+# Remove component base directory. (/var/opt/kuali/ruby)
+
+echo "[-uninstall-] Removing ${COMPONENT_BASE} ..."
+rm -rf ${COMPONENT_BASE}
+
+#
+# Remove component's initialization script.
+
+echo "[-uninstall-] Removing initialization script ..."
+rm -rf /etc/opt/kuali/__control__/profile.d/*ruby
