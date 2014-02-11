abfdd8badb25b65e3e12fb217c4a3977c542df03
Josh Shaloo <shaloo@email.arizona.edu>
2014-02-11 09:54:21 -0700

    #19 updating the liquibase_kc.sh to fix liquibase scripts

--- components/liquibase/liquibase-3.0.6/liquibase_kc.sh
+++ components/liquibase/liquibase-3.0.6/liquibase_kc.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
 LIQUIBASE_LIB_DIR=$LIQUIBASE_HOME/lib/
-LIQUIBASE_HOME=$LIQUIBASE_HOME/bin
+LIQUIBASE_HOME=$LIQUIBASE_HOME/bin/
+KC_ENV=$(cat /home/kualiadm/kitt-tools/.envrc)
 
 # grep the username, password, and url from the kc-security.xml file. This is the same
-# file that the KC application uses to connect to the DB so it should always be up to 
+# file that the KC application uses to connect to the DB so it should always be up to
 # date for the current environment.
-LIQUIBASE_DB_USERNAME=$(grep -e '<param name="datasource.username">' ~/kuali/main/dev/kc-security.xml  | sed -e 's/^[ \t]*<param name="datasource.username">//' -e 's/<\/param>.*$//')
-LIQUIBASE_DB_PASSWORD=$(grep -e '<param name="datasource.password">' ~/kuali/main/dev/kc-security.xml  | sed -e 's/^[ \t]*<param name="datasource.password">//' -e 's/<\/param>.*$//')
-LIQUIBASE_DB_URL=$(grep -e '<param name="datasource.url">' ~/kuali/main/dev/kc-security.xml  | sed -e 's/^[ \t]*<param name="datasource.url">//' -e 's/<\/param>.*$//')
+LIQUIBASE_DB_USERNAME=$(grep -e '<param name="datasource.username">' ~/kuali/main/$KC_ENV/kc-security.xml  | sed -e 's/^[ \t]*<param name="datasource.username">//' -e 's/<\/param>.*$//')
+LIQUIBASE_DB_PASSWORD=$(grep -e '<param name="datasource.password">' ~/kuali/main/$KC_ENV/kc-security.xml  | sed -e 's/^[ \t]*<param name="datasource.password">//' -e 's/<\/param>.*$//')
+LIQUIBASE_DB_URL=$(grep -e '<param name="datasource.url">' ~/kuali/main/$KC_ENV/kc-security.xml  | sed -e 's/^[ \t]*<param name="datasource.url">//' -e 's/<\/param>.*$//')
 
-exec $LIQUIBASE_HOME/bin/liquibase \
+exec $LIQUIBASE_HOME/liquibase \
 --url=$LIQUIBASE_DB_URL \
---username=$USERNAME \
+--username=$LIQUIBASE_DB_USERNAME \
 --password=$LIQUIBASE_DB_PASSWORD \
---classpath=$LIQUIBASE_LIB_DIR/ojdbc14.jar \
+--classpath=$LIQUIBASE_LIB_DIR/ojdbc6.jar \
 --driver=oracle.jdbc.driver.OracleDriver \
---logLevel=finest \
+--logLevel=debug \
 $@
